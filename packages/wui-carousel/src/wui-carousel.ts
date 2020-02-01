d76a504651e1b581213e643cc15eae1ce3cb7368
Muneer Zargar <zargarmuneer@yahoo.com>
2020-02-01 15:45:39 +0100

    setTimeOut to requestAnimationFrame

--- packages/wui-carousel/src/wui-carousel.ts
+++ packages/wui-carousel/src/wui-carousel.ts
@@ -1,86 +1,82 @@
-import {LitElement, customElement, html, css, property, TemplateResult} from 'lit-element';
-import {ListItem, ImageInterface} from './Interfaces/wui-carousel.interfaces';
+import { LitElement, customElement, html, css, property, TemplateResult } from 'lit-element';
+import { ListItem, ImageInterface } from './Interfaces/wui-carousel.interfaces';
 import './wui-carousel-wrapper/src/wui-carousel-wrapper';
 import './wui-carousel-navigation/src/wui-carousel-navigation';
 
 @customElement('wui-carousel')
-export class WuiCarousel extends LitElement{
-    
-    @property() carouselItems: ListItem[] = [] ;
-    @property() enableFullScreen: boolean = false;
-    @property() enableCounter: boolean = true;
-    @property() enableArrows: boolean = true;
-    @property() oldItemIndex: number = 0;
-    @property() indicatorType: string = '';
-    @property() maxItems: number | undefined;
-    private _itemIndex:number = 0;
-    public get itemIndex(): number {
-        return this._itemIndex;
-    }
-    public set itemIndex(value: number) {
-        const oldValue: number = this.itemIndex;
-        this.oldItemIndex = oldValue;
-        this._itemIndex = value;
-        this.requestUpdate('itemIndex', oldValue);
-    }
-    
-    static get styles() {
-        return css `
-            .wui-carousel {
-                display: block;
-            }
-        `
-    }
+export class WuiCarousel extends LitElement {
+  @property({ type: Array }) carouselItems: ListItem[] = [];
+  @property({ type: Boolean }) enableFullScreen = false;
+  @property({ type: Boolean }) enableCounter: any;
+  @property({ type: Boolean }) enableArrows: any;
+  @property({ type: Number }) oldItemIndex = 0;
+  @property({ type: String }) indicatorType = '';
+  @property({ type: Number }) maxItems: number | undefined;
+  private _itemIndex: number = 0;
+  public get itemIndex(): number {
+    return this._itemIndex;
+  }
+  public set itemIndex(value: number) {
+    const oldValue: number = this.itemIndex;
+    this.oldItemIndex = oldValue;
+    this._itemIndex = value;
+    this.requestUpdate('itemIndex', oldValue);
+  }
 
-    render(): TemplateResult {
-        return html `
-            <div class= "wui-carousel">
-                <wui-carousel-wrapper 
-                    .carouselItems= "${this.__getItems(this.carouselItems, 'wrapper')}"
-                    .currentIndex= "${this.itemIndex}"
-                    .prevIndex="${this.oldItemIndex}"
-                    .enableCounter= "${this.enableCounter}"
-                    .enableArrows= "${this.enableArrows}"
-                    @selected-item= "${this.__setSelectedIndex}"
-                >
-                </wui-carousel-wrapper>
-                <wui-carousel-navigation 
-                    .navigationItems= "${this.__getItems(this.carouselItems, 'navigationBottom')}"
-                    .currentItemIndex= "${this.itemIndex}"
-                    .indicatorType= "${this.indicatorType}"
-                    @selected-item= "${this.__setSelectedIndex}"
-                    exportparts="wui-carousel-custom-nav: wui-carousel-custom-nav"
-                    >
-                </wui-carousel-navigation>
-            </div>
-        `
-    }
+  static get styles() {
+    return css`
+      .wui-carousel {
+        display: block;
+      }
+    `;
+  }
 
-    private __setSelectedIndex(event: {detail: {selectedIndex: number}}): void {
-        this.itemIndex = event.detail.selectedIndex;
-    }
+  render(): TemplateResult {
+    return html`
+      <div class="wui-carousel">
+        <wui-carousel-wrapper
+          .carouselItems="${this.__getItems(this.carouselItems, 'wrapper')}"
+          .currentIndex="${this.itemIndex}"
+          .prevIndex="${this.oldItemIndex}"
+          .enableCounter="${this.enableCounter}"
+          .enableArrows="${this.enableArrows}"
+          @selected-item="${this.__setSelectedIndex}"
+        >
+        </wui-carousel-wrapper>
+        <wui-carousel-navigation
+          .navigationItems="${this.__getItems(this.carouselItems, 'navigationBottom')}"
+          .currentItemIndex="${this.itemIndex}"
+          .indicatorType="${this.indicatorType}"
+          @selected-item="${this.__setSelectedIndex}"
+          exportparts="wui-carousel-custom-nav: wui-carousel-custom-nav"
+        >
+        </wui-carousel-navigation>
+      </div>
+    `;
+  }
 
-   //TODO: Fix return type here .
+  private __setSelectedIndex(event: { detail: { selectedIndex: number } }): void {
+    this.itemIndex = event.detail.selectedIndex;
+  }
 
-    private __getItems(items : ListItem [] = [], listType: string) {
-        const maxAllowed: number =  this.maxItems ?? 4;
-        const renderedList  = items.slice(0, maxAllowed);
-        return renderedList.map((item: ListItem) => {
-            if(listType === 'wrapper') {
-                const {type, src, altText} = item;
-                const listItem : ListItem = {
-                    type,
-                    src, 
-                    altText
-                };
-                return listItem;
-            }
-            else if(listType === 'navigationBottom') {
-                const defaultNav: ImageInterface = {src: '', altText: 'null'} 
-                return item.thumbnail ?? defaultNav;
-            }
-        });
-    }
-    
+  //TODO: Fix return type here .
+
+  private __getItems(items: ListItem[] = [], listType: string) {
+    const maxAllowed: number = this.maxItems ?? 4;
+    const renderedList = items.slice(0, maxAllowed);
+    return renderedList.map((item: ListItem) => {
+      if (listType === 'wrapper') {
+        const { type, src, altText } = item;
+        const listItem: ListItem = {
+          type,
+          src,
+          altText,
+        };
+        return listItem;
+      } else if (listType === 'navigationBottom') {
+        const defaultNav: ImageInterface = { src: '', altText: 'null' };
+        return item.thumbnail ?? defaultNav;
+      }
+    });
+  }
 }
-
